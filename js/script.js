35b435034147a7c112fbec514f08a44ff1b44f7b
TobonSergio <tobonsergio22@gmail.com>
2023-09-22 12:02:48 -0500

    commit de la parte de abaout me junto a primer JavaScrip dando un par de funciones para ocultar y aparecer informacion

---
+++ js/script.js
@@ -0,0 +1,51 @@
+var contenidoSkills = document.getElementById("contenidoSkills");
+var contenidoExperience = document.getElementById("contenidoExperience");
+var contenidoEducation = document.getElementById("contenidoEducation");
+
+document.getElementById("skills").addEventListener("click", function(){
+
+    contenidoExperience.style.display = "none";
+    contenidoEducation.style.display = "none";
+
+    if(contenidoSkills.style.display === "none" || contenidoSkills.style.display === ""){
+
+        contenidoSkills.style.display = "block";
+
+    }else{
+
+        contenidoSkills.style.display = "none";
+    
+    }
+});
+
+document.getElementById("experience").addEventListener("click", function(){
+
+    contenidoSkills.style.display = "none";
+    contenidoEducation.style.display = "none";
+
+    if(contenidoExperience.style.display === "none" || contenidoExperience.style.display === ""){
+
+        contenidoExperience.style.display = "block";
+
+    }else{
+
+        contenidoExperience.style.display = "none";
+        
+    }
+});
+
+document.getElementById("education").addEventListener("click", function(){
+
+    contenidoSkills.style.display = "none";
+    contenidoExperience.style.display = "none";
+
+    if(contenidoEducation.style.display === "none" || contenidoEducation.style.display === ""){
+
+        contenidoEducation.style.display = "block";
+
+    }else{
+
+        contenidoEducation.style.display = "none";
+        
+    }
+});
